07dd27356da277aebdb67574d23cd96ca57f287c
Hannele Ruiz <justlemoncl@gmail.com>
2022-05-19 18:56:05 -0400

    Added PointF like class

---
+++ LemonUI.SHV/PointF.cpp
@@ -0,0 +1,35 @@
+﻿#include "PointF.hpp"
+
+namespace LemonUI
+{
+    PointF::PointF(const float x, const float y)
+    {
+        x_ = x;
+        y_ = y;
+    }
+
+    bool PointF::IsEmpty()
+    {
+        return x_ == 0 && y_ == 0;
+    }
+
+    float PointF::GetX() const
+    {
+        return x_;
+    }
+
+    float PointF::GetY() const
+    {
+        return y_;
+    }
+
+    void PointF::SetX(const float x)
+    {
+        x_ = x;
+    }
+    
+    void PointF::SetY(const float y)
+    {
+        y_ = y;
+    }
+}
---
+++ LemonUI.SHV/PointF.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace LemonUI
+{
+    class PointF
+    {
+    private:
+        float x_;
+        float y_;
+    public:
+        PointF(float x, float y);
+        bool IsEmpty();
+        float GetX() const;
+        float GetY() const;
+        void SetX(float x);
+        void SetY(float y);
+    };
+}
