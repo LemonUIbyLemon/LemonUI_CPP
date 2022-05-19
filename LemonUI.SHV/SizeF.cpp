9cd62cd45325ecf030bf78846043957ba2cdfa4e
Hannele Ruiz <justlemoncl@gmail.com>
2022-05-19 19:01:20 -0400

    Added SizeF like class

---
+++ LemonUI.SHV/SizeF.cpp
@@ -0,0 +1,35 @@
+﻿#include "SizeF.hpp"
+
+namespace LemonUI
+{
+    SizeF::SizeF(const float width, const float height)
+    {
+        width_ = width;
+        height_ = height;
+    }
+
+    bool SizeF::IsEmpty()
+    {
+        return width_ == 0 && height_ == 0;
+    }
+
+    float SizeF::GetWidth() const
+    {
+        return width_;
+    }
+
+    float SizeF::GetHeight() const
+    {
+        return height_;
+    }
+
+    void SizeF::SetWidth(const float width)
+    {
+        width_ = width;
+    }
+    
+    void SizeF::SetHeight(const float height)
+    {
+        height_ = height;
+    }
+}
---
+++ LemonUI.SHV/SizeF.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace LemonUI
+{
+    class SizeF
+    {
+    private:
+        float width_;
+        float height_;
+    public:
+        SizeF(float width, float height);
+        bool IsEmpty();
+        float GetWidth() const;
+        float GetHeight() const;
+        void SetWidth(float width);
+        void SetHeight(float height);
+    };
+}
