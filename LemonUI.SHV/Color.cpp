c704bbce970a37d23fd4818ee5fc21d56ea0c3bf
Hannele Ruiz <justlemoncl@gmail.com>
2022-05-19 19:54:14 -0400

    Added color class

---
+++ LemonUI.SHV/Color.cpp
@@ -0,0 +1,61 @@
+﻿#include "Color.hpp"
+
+namespace LemonUI
+{
+    Color::Color(const std::byte r, const std::byte g, const std::byte b)
+    {
+        a_ = static_cast<std::byte>(0);
+        r_ = r;
+        g_ = g;
+        b_ = b;
+    }
+    
+    Color::Color(const std::byte a, const std::byte r, const std::byte g, const std::byte b)
+    {
+        a_ = a;
+        r_ = r;
+        g_ = g;
+        b_ = b;
+    }
+
+    std::byte Color::GetA() const
+    {
+        return a_;
+    }
+
+    std::byte Color::GetR() const
+    {
+        return r_;
+    }
+
+    std::byte Color::GetG() const
+    {
+        return g_;
+    }
+
+    std::byte Color::GetB() const
+    {
+        return b_;
+    }
+
+    void Color::SetA(const std::byte a)
+    {
+        a_ = a;
+    }
+
+    void Color::SetR(const std::byte r)
+    {
+        r_ = r;
+    }
+
+    void Color::SetG(const std::byte g)
+    {
+        g_ = g;
+    }
+
+    void Color::SetB(const std::byte b)
+    {
+        b_ = b;
+    }
+
+}
---
+++ LemonUI.SHV/Color.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <cstddef>
+
+namespace LemonUI
+{
+    class Color
+    {
+    private:
+        std::byte a_;
+        std::byte r_;
+        std::byte g_;
+        std::byte b_;
+    public:
+        Color(std::byte r, std::byte g, std::byte b);
+        Color(std::byte a, std::byte r, std::byte g, std::byte b);
+        std::byte GetA() const;
+        std::byte GetR() const;
+        std::byte GetG() const;
+        std::byte GetB() const;
+        void SetA(std::byte a);
+        void SetR(std::byte r);
+        void SetG(std::byte g);
+        void SetB(std::byte b);
+    };
+}
